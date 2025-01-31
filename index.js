b569e37bd4fa09e4329588fe5d8bdc0737de9de6
fatihbaydar <fatihbaydar2004@gmail.com>
2025-01-31 18:48:36 +0300

    db&errorhandler

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 "use strict"
 
+const express = require("express")
+const app = express()
+/* ------------------------------------------------------- */
 
+//? REQUIRED MODULES
 
+require("dotenv").config()
+const HOST = process.env?.HOST || "127.0.0.1"
+const PORT = process.env?.PORT || 8000
+/* ------------------------------------------------------- */
+
+//? CONFIGURATION
+
+const {dbConnection} = require("./src/configs/dbConnection")
+dbConnection()
+/* ------------------------------------------------------- */
+
+//? MIDDLEWARES
+
+/* ------------------------------------------------------- */
+
+//? ROUTES
+
+// HomePath
+app.all("/", (req,res) => {
+    res.send({
+        error:false,
+        message:"Welcome to HOTEL API",
+        documents:{
+            swagger:"/documents/swagger",
+            redoc:"/documents/redoc",
+            json:"/documents/json"
+        },
+        user:req.user
+    })
+})
+/* ------------------------------------------------------- */
+
+//? ERROR HANDLER
+app.use(require("./src/middlewares/errorHandler"))
+/* ------------------------------------------------------- */
+
+//? RUN SERVER
+app.listen(PORT, HOST, () => console.log(`running:http://${HOST}:${PORT}`))
