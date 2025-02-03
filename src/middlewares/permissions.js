8ab7be2999953ef1cf10d1d4264fe6c5f2021594
fatihbaydar <fatihbaydar2004@gmail.com>
2025-02-03 22:09:27 +0300

    permission&authentication

---
+++ src/middlewares/permissions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("No permission : You must login.");
+    }
+  },
+
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("No permission : You must login and to be Admin.");
+    }
+  },
+};
